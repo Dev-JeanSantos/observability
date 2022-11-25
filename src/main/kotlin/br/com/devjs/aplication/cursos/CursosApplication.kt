a7bf50ea15ae700283d58cbc5f49dbc4de04a4ec
Dev-JeanSantos <jeancbsan@gmail.com>
2022-11-25 16:49:16 -0300

    Criação do projeto com observability (Grafana Loki)

---
+++ src/main/kotlin/br/com/devjs/aplication/cursos/CursosApplication.kt
@@ -0,0 +1,11 @@
+package br.com.devjs.aplication.cursos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CursosApplication
+
+fun main(args: Array<String>) {
+	runApplication<CursosApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/devjs/aplication/cursos/CursosApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.devjs.aplication.cursos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CursosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
