a1cb3b407ab337d17789938c337ed329bb3d328f
Dev-JeanSantos <jeancbsan@gmail.com>
2022-11-29 10:49:12 -0300

    refatoração

--- src/main/kotlin/br/com/devjs/aplication/cursos/adapter/web/v1/controllers/CursoController.kt
+++ src/main/kotlin/br/com/devjs/aplication/cursos/adapter/web/v1/controllers/CursoController.kt
@@ -10,9 +10,11 @@ import br.com.devjs.aplication.cursos.application.exceptions.ResourcesNotFoundEx
 import br.com.devjs.aplication.cursos.application.ports.input.CourseUseCase
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["*"], maxAge = 3600)
 class CursoController(
     @Autowired
     private val courseUseCase: CourseUseCase
